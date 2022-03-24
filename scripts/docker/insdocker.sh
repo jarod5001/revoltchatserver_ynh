c99bcf130b9a00c307e15af95d12ec6819f1c6cb
jarod5001 <68397534+jarod5001@users.noreply.github.com>
2022-03-24 22:52:30 +0100

    Update insdocker.sh

--- scripts/docker/insdocker.sh
+++ scripts/docker/insdocker.sh
@@ -6,5 +6,6 @@ sudo apt install docker-ce
 
 sudo usermod -aG docker ${USER}
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
